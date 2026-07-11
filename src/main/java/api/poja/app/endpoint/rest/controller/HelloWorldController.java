8464a3cad64fad4f28cfeadb96d7ba87553d4eb1
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-07-11 11:17:54 +0300

    feat(event): add async email sending via SendEmailRequested event

--- src/main/java/api/poja/app/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/api/poja/app/endpoint/rest/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package api.poja.app.endpoint.rest.controller;
 
+import api.poja.app.endpoint.event.EventProducer;
+import api.poja.app.endpoint.event.model.SendEmailRequested;
+import api.poja.app.mail.Mailer;
 import api.poja.app.service.HelloWorldService;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class HelloWorldController {
   private final HelloWorldService service;
+  private final Mailer mailer;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
-  @GetMapping("/hello")
+  @GetMapping("/allo")
   public String helloWorld(@RequestParam String name) {
     return service.uploadHelloWorldMessage(name);
+  }
+
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String helloWorld1(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "... world!";
   }
 }
